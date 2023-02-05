c5594cbeb501b254122fda6e081a2535dd8a2b8a
Scarlett Vu <uscarlettv@gmail.com>
2023-02-04 23:58:44 -0800

    Initial Commit

---
+++ Clock App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock App
+//
+//  Created by Scarlett  on 2/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
